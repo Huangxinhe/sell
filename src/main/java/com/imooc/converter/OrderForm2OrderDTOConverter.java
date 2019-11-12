d604798a0ca32f2727c7fa00157807e4c026a9cf
huangxinhe <946840567@qq.com>
2019-11-12 23:16:01 +0800

    创建商品controller

--- src/main/java/com/imooc/converter/OrderForm2OrderDTOConverter.java
+++ src/main/java/com/imooc/converter/OrderForm2OrderDTOConverter.java
@@ -1,6 +1,7 @@
 package com.imooc.converter;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import com.imooc.dataobject.OrderDetail;
 import com.imooc.dto.OrderDTO;
@@ -8,6 +9,7 @@ import com.imooc.enums.ResultEnum;
 import com.imooc.exception.SellException;
 import com.imooc.form.OrderForm;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,24 +23,24 @@ public class OrderForm2OrderDTOConverter {
 
     public static OrderDTO convert(OrderForm orderForm) {
         Gson gson = new Gson();
-        OrderDTO orderDTO = new OrderDTO();
 
+        OrderDTO orderDTO = new OrderDTO();
         orderDTO.setBuyerName(orderForm.getName());
         orderDTO.setBuyerPhone(orderForm.getPhone());
         orderDTO.setBuyerAddress(orderForm.getAddress());
         orderDTO.setBuyerOpenid(orderForm.getOpenid());
-
-        List<OrderDetail> orderDetailList = new ArrayList<>();
+        List<OrderDetail> orderDetailList;
         try {
             orderDetailList = gson.fromJson(orderForm.getItems(),
                     new TypeToken<List<OrderDetail>>() {
                     }.getType());
         } catch (Exception e) {
-            log.error("【对象转换】错误, string={}", orderForm.getItems());
+            log.error("【对象转换】错误，string={}",orderForm.getItems());
+            e.printStackTrace();
             throw new SellException(ResultEnum.PARAM_ERROR);
         }
-        orderDTO.setOrderDetailList(orderDetailList);
 
+        orderDTO.setOrderDetailList(orderDetailList);
         return orderDTO;
     }
 }
